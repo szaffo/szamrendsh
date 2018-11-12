34f48232fc9284fee8500bfc9cd72d90f7767441
Szabó <martin77szabo@gmail.com>
2018-11-12 19:43:39 +0100

    help done

--- html.sh
+++ html.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 case $1 in
 	-st)
-	echo "Kapcsoló: st";;
+	echo "Kapcsoló: st"
+	;;
+	
 	-pc)
-	echo "Kapcsoló: pc";;
+	echo "Kapcsoló: pc"
+	;;
 
 	-pr)
-	echo "Kapcsoló: pr";;
+	echo "Kapcsoló: pr"
+	;;
 
 	-pd)
-	echo "Kapcsoló: pd";;
+	echo "Kapcsoló: pd"
+	;;
 
 	*)
-	echo "Kapcsoló: *";;
+	echo "Nem adott meg érvényes kapcsolót"
+	echo "-st (showtitle) kapcsolóval megmutatja a Title címke tartalmát!"
+	echo "-pc (picture count) kapcsolóval meg tudjuk mondani, hogy hány képet használunk és mennyi azok mérete!"
+	echo "-pr (picture replace) egy adott képet kicserél egy másikra, html.sh -pr alma.jpg barack.png"
+	echo "-pd (picture delete) egy adott képet kitöröl az állományból."
+	;;
 esac
