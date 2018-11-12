d4804392995029b2276899c32aceebe3569679a5
Szabó <martin77szabo@gmail.com>
2018-11-12 20:08:52 +0100

    it ask for file, and -h and -help no more exist

--- html.sh
+++ html.sh
@@ -18,11 +18,19 @@ case $1 in
 
 	*)
 	echo "Nem adott meg érvényes kapcsolót"
-	-h)
-	-help)
+	echo "parancs kapcsolók fájl"
 	echo "-st (showtitle) kapcsolóval megmutatja a Title címke tartalmát!"
 	echo "-pc (picture count) kapcsolóval meg tudjuk mondani, hogy hány képet használunk és mennyi azok mérete!"
 	echo "-pr (picture replace) egy adott képet kicserél egy másikra, html.sh -pr alma.jpg barack.png"
 	echo "-pd (picture delete) egy adott képet kitöröl az állományból."
+	exit
 	;;
+
 esac
+
+if [ -z $2 ]; then
+	echo "Kérlek adj meg egy létező fájlnevet"
+	exit
+fi
+
+echo "Done"
