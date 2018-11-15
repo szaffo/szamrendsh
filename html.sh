6dce0925ee1a105bedc68c533a6ad0c283d0d128
Your Name <you@example.com>
2018-11-15 17:20:41 +0100

    html.sh updated on master, ready for uploa

--- html.sh
+++ html.sh
@@ -1,38 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 if [ -z $2 ]; then
-	echo "Kérlek adj meg egy létező fájlnevet"
-	echo "parancs kapcsolók fájl"
 	exit
 fi
 
 case $1 in
-	-st)
-	echo "Kapcsoló: st"
-	cat $2 | grep -i title
+    -st)
+	showTitle=`sed -i 's/^ *//' $2 | cat $2 | grep "<title>"`
+    echo "${showTitle:7:-8}"
 	;;
 	
 	-pc)
-	echo "Kapcsoló: pc"
-	cat $2 | grep -i '<img\>' | wc -l
+    pictureCount=`cat $2 | grep -i "<img" | wc -l`
+    echo $pictureCount
 	;;
 
 	-pr)
-	echo "Kapcsoló: pr"
+    if [ $# -lt 4 ]; then
+        exit
+    fi
+    sed -i "s/$3/$4/g" "$2"
 	;;
 
 	-pd)
-	echo "Kapcsoló: pd"
+	picture=`grep -i "<img" $2 | sed  -n $3p`
+    changedHTML=`grep -v "$picture" "$2"`
+    echo $changedHTML > $2
 	;;
-
-	*)
-	echo "Nem adott meg érvényes kapcsolót"
-	echo "parancs kapcsolók fájl"
-	echo "-st (showtitle) kapcsolóval megmutatja a Title címke tartalmát!"
-	echo "-pc (picture count) kapcsolóval meg tudjuk mondani, hogy hány képet használunk és mennyi azok mérete!"
-	echo "-pr (picture replace) egy adott képet kicserél egy másikra, html.sh -pr alma.jpg barack.png"
-	echo "-pd (picture delete) egy adott képet kitöröl az állományból."
-	exit
-	;;
-
 esac
 
